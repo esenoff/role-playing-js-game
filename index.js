f3ebb66e6b3d1096e6a2d19ef217d071f8c2d4c0
Serdar Esenov <83946397+esenoff@users.noreply.github.com>
2022-07-18 18:37:38 +0300

    map over diceRoll

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hero = {
    name : "Wizard",
    avatar : "images/wizard.jpg",
    health : 60,
-   dice : 6
+   diceRoll: [3, 1, 4],
+   diceCount: 3
 }
 
 const monster = {
@@ -11,18 +12,23 @@ const monster = {
    name : "Orc",
    avatar : "images/orc.jpg",
    health : 10,
-   dice : 4
+   diceRoll: [6],
+   diceCount: 1
 }
 
 function renderCharacter(data){
-      const {id, name, avatar, health, dice} = data;
-         return document.getElementById(id).innerHTML = 
+      const {id, name, avatar, health, dice, diceRoll} = data;
+
+      const diceHtml = diceRoll.map(num => `<div class="dice">${num}</div>`).join('');
+      
+      document.getElementById(id).innerHTML = 
          `<div class="character-card"> 
          <h4 class="name"> ${name} </h4>
          <img class="avatar" src="${avatar}"/> 
          <p class="health">health: <b> ${health} </b></p> 
          <div class="dice-container">
-         <div class="dice"> ${dice} </div></div>
+            ${diceHtml}
+         </div>
          </div>`
 }
 
