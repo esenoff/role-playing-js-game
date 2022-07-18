b8f14ce9c64528f34fa2e5d5029a50da3fafd2c6
Serdar Esenov <83946397+esenoff@users.noreply.github.com>
2022-07-18 14:39:49 +0300

    modify renderCharacter

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+const hero = {
+   id : "hero",
+   name : "Wizard",
+   avatar : "images/wizard.jpg",
+   health : 60,
+   dice : 6
+}
 
-const heroId = "hero";
-const heroName = "Wizard";
-const heroAvatar = "images/wizard.jpg";
-const heroHealth = 60;
-const heroDiceRoll = 6;
+const monster = {
+   id : "monster",
+   name : "Orc",
+   avatar : "images/orc.jpg",
+   health : 10,
+   dice : 4
+}
 
-
-const monsterId = "monster";
-const monsterName = "Orc";
-const monsterAvatar = "images/orc.jpg";
-const monsterHealth = 10;
-const monsterDiceRoll = 4;
-
-function renderCharacter(id, name, avatar, health, dice){
-         return document.getElementById(id).innerHTML = 
+function renderCharacter(data){
+         return document.getElementById(data.id).innerHTML = 
          `<div class="character-card"> 
-         <h4 class="name"> ${name} </h4>
-         <img class="avatar" src="${avatar}"/> 
-         <p class="health">health: <b> ${health} </b></p> 
+         <h4 class="name"> ${data.name} </h4>
+         <img class="avatar" src="${data.avatar}"/> 
+         <p class="health">health: <b> ${data.health} </b></p> 
          <div class="dice-container">
-         <div class="dice"> ${dice} </div></div>
+         <div class="dice"> ${data.dice} </div></div>
          </div>`
 }
 
-renderCharacter(heroId, heroName, heroAvatar, heroHealth, heroDiceRoll);
-renderCharacter(monsterId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll);
+renderCharacter(hero);
+renderCharacter(monster);
