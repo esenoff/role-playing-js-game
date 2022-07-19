4fc647986abefc78ab5b114d3f92a766c93924d8
Serdar Esenov <83946397+esenoff@users.noreply.github.com>
2022-07-19 10:48:35 +0300

    use fill method instead of the for loop

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ const monster = {
 }
 
 function getDiceRollArray(diceCount){
-   let newDiceRolls = [];
-   for(let i=0; i<diceCount; i++){
-      newDiceRolls.push(Math.floor(Math.random()*6)+1)
-   }
-  return newDiceRolls;
+
+   return new Array(diceCount).fill(0).map(()=>Math.floor(Math.random()*6)+1)
+//    let newDiceRolls = [];
+//    for(let i=0; i<diceCount; i++){
+//       newDiceRolls.push(Math.floor(Math.random()*6)+1)
+//    }
+//   return newDiceRolls;
 }
 
 function getDiceHtml(diceCount){
